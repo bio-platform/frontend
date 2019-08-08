96fe8a3dbb82a6a3817ff5bac34cc01199794f75
Tomas Palco <xpalco@fi.muni.cz>
2019-08-08 14:16:18 +0200

    dataService working correctly, local api isnt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { UserOverviewComponent } from './user-overview/user-overview.component';
 
 import { DataService } from './data.service';
 import { MessagesComponent } from './messages/messages.component';
+import { NavbarComponent } from './navbar/navbar.component';
 
 @NgModule({
   declarations: [
@@ -22,7 +23,8 @@ import { MessagesComponent } from './messages/messages.component';
     DashboardComponent,
     HeaderComponent,
     UserOverviewComponent,
-    MessagesComponent
+    MessagesComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
--- src/app/user-overview/user-overview.component.ts
+++ src/app/user-overview/user-overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Label, Color } from 'ng2-charts';
 
 @Component({
   selector: 'app-user-overview',
@@ -10,6 +11,10 @@ export class UserOverviewComponent implements OnInit {
   public pieChartLabels = ['Used', 'Available'];
   public pieChartData = [200, 150];
   public pieChartType = 'pie';
+  public pieChartColors: Array < any > = [{
+    backgroundColor: ['#607D8B', 'rgba(148,159,177,0.2)'],
+    
+ }];
 
   constructor() { }
 
