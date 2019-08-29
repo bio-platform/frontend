8be7c80a73078c326ade3cb3609b3a7e9da78149
Tomáš Palčo <xpalco@fi.muni.cz>
2019-08-29 13:01:28 +0200

    change to api syntax, added / after apiURL

--- src/app/data.service.ts
+++ src/app/data.service.ts
@@ -13,7 +13,7 @@ import { Router } from '@angular/router';
   providedIn: 'root'
 })
 export class DataService {
-  private apiURL = "http://localhost:80/api";
+  private apiURL = "http://localhost:80/api/";
   private headers = new HttpHeaders();
   private response = new HttpResponse();
   
@@ -39,23 +39,23 @@ export class DataService {
   getLimit (): Observable<Limit> {
      
       this.messageService.add('Dataservice: getLimit');
-      return this.httpClient.get<Limit>(this.apiURL+"/limits/");
+      return this.httpClient.get<Limit>(this.apiURL+"limits/");
   }
 
   //getNetwork (): Observable<Network[]> {
     //this.messageService.add('Dataservice: getNetwork');
-    //return this.httpClient.get<Network[]>(this.apiURL+"/network/");
+    //return this.httpClient.get<Network[]>(this.apiURL+"network/");
 //}
 
   /*postInstance():void{
-    this.httpClient.post(this.apiURL,instanceData,{observe: 'response'}).toPromise().then((data:any) => {
+    this.httpClient.post(this.apiURL/instances,instanceData,{observe: 'response'}).toPromise().then((data:any) => {
       console.log('isLogged = true');
       this.router.navigate(['dashboard']);
 
     },
     (err:any) =>{
       console.log(err);
-    });8?
+    });
     
   }*/
    
