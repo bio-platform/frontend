dd059c4c7edb76919f78c3de40f8b680e3b75375
Andrej Čermák <xcermak5@fi.muni.cz>
2020-09-04 14:44:02 +0200

    Update login.component.ts

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -27,7 +27,7 @@ export class LoginComponent implements OnInit {
       scope: 'openid profile email eduperson_entitlement',
       authority: 'https://login.cesnet.cz/oidc/',
       client_id: 'ca73360a-c510-4bc0-afb5-f5c5eee603ca',
-      redirect_uri: 'https://bio-portal.metacentrum.cz/callback',
+      redirect_uri: 'http://bio-portal.metacentrum.cz/callback',
       post_logout_redirect_uri: 'https://bio-portal.metacentrum.cz/',
     })
     
