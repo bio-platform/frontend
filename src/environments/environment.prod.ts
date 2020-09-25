ccb029f1afcd91d67a6f0ca60bbf053d5de578e9
Andrej Čermák <andrej.cermak.1999@gmail.com>
2020-09-25 12:28:50 +0200

    FIXED prod callback to https

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,7 +1,7 @@
 export const environment = {
   production: true,
   enviromentName: 'prod',
-  callback: 'http://bio-portal.metacentrum.cz/callback/',
+  callback: 'https://bio-portal.metacentrum.cz/callback/',
   apiUrl: 'https://bio-portal.metacentrum.cz/api/',
   postLogoutUrl: 'https://bio-portal.metacentrum.cz/'
 };
